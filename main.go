04a32ceee243e6495416c68bc6d034b6dd25aca4
Isidrik <talanlexa@gmail.com>
2023-05-17 00:41:55 +0300

    Add stuct for input, copy-pasted some code for creating files and add them to zip, switched to web-server for handle json and send zip back

--- main.go
+++ main.go
@@ -2,227 +2,424 @@ package main
 
 import (
 	pdf "Breeding/pdf"
+	"archive/zip"
+	"encoding/json"
 	"fmt"
+	"io"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+	"strconv"
+	"time"
 )
 
 func main() {
 
-	r := pdf.NewRequestPdf("")
+	//r := pdf.NewRequestPdf("")
 
-	father := Dog{
-		Type:     "среднеазиатская овчарка",
-		Nickname: "Dastan",
-		Sex:      "к",
-		Stamp:    "2139",
-		Pedigree: "UKU.R.0047773",
-		Owner: &Owner{
-			FIO:     "Кузнецов А.А.",
-			Contact: "Донецк, Флотская, 68",
-		},
+	//father := Dog{
+	//	Type:     "среднеазиатская овчарка",
+	//	Nickname: "Dastan",
+	//	Sex:      "к",
+	//	Stamp:    "2139",
+	//	Pedigree: "UKU.R.0047773",
+	//	Owner: &Owner{
+	//		FIO:     "Кузнецов А.А.",
+	//		Contact: "Донецк, Флотская, 68",
+	//	},
+	//}
+	//
+	//mother := Dog{
+	//	Type:     "среднеазиатская овчарка",
+	//	Nickname: "Анзурат Майсун Ф-Айпери",
+	//	Sex:      "c",
+	//	Stamp:    "CKF 4707",
+	//	Pedigree: "5582067 Р",
+	//	Owner: &Owner{
+	//		FIO:     "Каратаева Е.В",
+	//		Contact: "142660 МО, Орехово-Зуевский р-н, Савостьяново, 66",
+	//	},
+	//	Breeder: &Breeder{
+	//		FIO:     "Каратаева Е.В",
+	//		Name:    "Анзурат Майсун",
+	//		Contact: "142660 МО, Орехово-Зуевский р-н, Савостьяново, 66",
+	//		Email:   "breeder@email.com",
+	//	},
+	//}
+	//
+	//gaplan := Puppy{
+	//	Dog: Dog{
+	//		Type:     "среднеазиатская овчарка",
+	//		Nickname: "Гаплан",
+	//		Sex:      "к",
+	//		Stamp:    "CKF 6059",
+	//		Pedigree: "",
+	//		Owner: &Owner{
+	//			FIO:     " ",
+	//			Contact: " ",
+	//		},
+	//		Breeder: &Breeder{
+	//			FIO:     "Каратаева Е.В",
+	//			Contact: "142660 МО, Орехово-Зуевский р-н, Савостьяново, 66",
+	//			Name:    "Анзурат Майсун",
+	//			Email:   "tula-skif@mail.ru",
+	//		},
+	//		Father: &father,
+	//		Mother: &mother,
+	//	},
+	//	Color:    "бел-чер",
+	//	Birthday: "28.03.2023",
+	//	WoolType: "",
+	//	Description: struct {
+	//		Comment  string
+	//		Defect   bool
+	//		Revision int
+	//	}{"Замечаний нет", false, 0},
+	//	IncreaseIndex: func(a int) int {
+	//		return a + 1
+	//	},
+	//}
+	//
+	//galsan := Puppy{
+	//	Dog: Dog{
+	//		Type:     "среднеазиатская овчарка",
+	//		Nickname: "Гаплан",
+	//		Sex:      "к",
+	//		Stamp:    "CKF 6060",
+	//		Pedigree: "",
+	//		Owner: &Owner{
+	//			FIO:     "Каратаева Е.В",
+	//			Contact: "142660 МО, Орехово-Зуевский р-н, Савостьяново, 66",
+	//		},
+	//	},
+	//	Color:    "бел-чер",
+	//	Birthday: "28 марта 2023",
+	//	WoolType: "",
+	//	Description: struct {
+	//		Comment  string
+	//		Defect   bool
+	//		Revision int
+	//	}{"Замечаний нет", false, 0},
+	//	IncreaseIndex: func(a int) int {
+	//		return a + 1
+	//	},
+	//}
+	//
+	//ganzhin := Puppy{
+	//	Dog: Dog{
+	//		Type:     "среднеазиатская овчарка",
+	//		Nickname: "Гаплан",
+	//		Sex:      "с",
+	//		Stamp:    "CKF 6064",
+	//		Pedigree: "",
+	//		Owner: &Owner{
+	//			FIO:     "Каратаева Е.В",
+	//			Contact: "142660 МО, Орехово-Зуевский р-н, Савостьяново, 66",
+	//		},
+	//	},
+	//	Color:    "бел-чер",
+	//	Birthday: "28 марта 2023",
+	//	WoolType: "",
+	//	Description: struct {
+	//		Comment  string
+	//		Defect   bool
+	//		Revision int
+	//	}{"Замечаний нет", false, 0},
+	//	IncreaseIndex: func(a int) int {
+	//		return a + 1
+	//	},
+	//}
+	//
+	//skiff := DocumentCenter{
+	//	Name:         "ТООО «Кинологический центр «СКИФ» г. Тула",
+	//	Email:        "tula-skif@mail.ru",
+	//	Mobile:       "+7 910 701-55-99",
+	//	Federation:   "РФСС",
+	//	FolderNumber: "7107",
+	//	Address:      "300025 Тула, проспект Ленина, 104-201",
+	//	StumpCode:    "CKF",
+	//}
+	//
+	//instructor := Personal{
+	//	"Девятиярова Е.А.",
+	//	"Тульская обл, Ленинский р-н, Нижняя Китаевка, 24",
+	//}
+	//
+	//stumpMan := Personal{"Аргунова Л.А.", ""}
+	//
+	//data := Mating{
+	//	Identification: "31 декабря 2022",
+	//	First:          "01 января 2023",
+	//	Control:        "02 января 2023",
+	//	Address:        "Донецк, Флотская, 68",
+	//	Male:           &father,
+	//	Female:         &mother,
+	//	DCenter:        &skiff,
+	//	Personal:       &instructor,
+	//}
+	//
+	//litter1 := Litter{
+	//	Stump:    "CKF",
+	//	ActDate:  "20 августа 1944",
+	//	Birthday: "16 декабря 2016",
+	//	Control:  "",
+	//	Address:  "",
+	//	Male:     &father,
+	//	Female:   &mother,
+	//	Puppy:    []*Puppy{&gaplan, &galsan, &ganzhin},
+	//	DCenter:  &skiff,
+	//	Personal: &instructor,
+	//	Claim:    &stumpMan,
+	//	Chief:    &instructor,
+	//}
+	//
+	//gaplanPC := PuppyCard{
+	//	Puppy:   gaplan,
+	//	DCenter: skiff,
+	//}
+
+	//templatePath := "blankMating.html"
+	////templatePath := "blank4.html"
+	//outputPath := fmt.Sprintf("%s_акт_вязки_%s.pdf", data.Personal.FIO, data.Female.Nickname)
+	//if err := r.ParseTemplate(templatePath, data); err == nil {
+	//	args := []string{"no-pdf-compression"}
+	//	ok, _ := r.GeneratePDF(outputPath, args)
+	//	fmt.Println(ok, "pdf generated successfully")
+	//} else {
+	//	fmt.Println(err)
+	//}
+	//
+	//templatePath = "blankAgreement.html"
+	////templatePath := "blank4.html"
+	//outputPath = fmt.Sprintf("%s_согласие_ПД_%s.pdf", data.Personal.FIO, data.Female.Nickname)
+	//if err := r.ParseTemplate(templatePath, data); err == nil {
+	//	args := []string{"no-pdf-compression"}
+	//
+	//	ok, _ := r.GeneratePDF(outputPath, args)
+	//	fmt.Println(ok, "pdf generated successfully")
+	//} else {
+	//	fmt.Println(err)
+	//}
+	//
+	//templatePath = "blankSurvey.html"
+	////templatePath := "blank4.html"
+	//outputPath = fmt.Sprintf("%s_обследование_%s.pdf", litter1.Personal.FIO, litter1.Birthday)
+	//if err := r.ParseTemplate(templatePath, litter1); err == nil {
+	//	args := []string{"no-pdf-compression"}
+	//	ok, _ := r.GeneratePDF(outputPath, args)
+	//	fmt.Println(ok, "pdf generated successfully")
+	//} else {
+	//	fmt.Println(err)
+	//}
+	//
+	//templatePath = "blankRegister.html"
+	////templatePath := "blank4.html"
+	//outputPath = fmt.Sprintf("%s_регистрация_%s.pdf", litter1.Personal.FIO, litter1.Birthday)
+	//if err := r.ParseTemplate(templatePath, litter1); err == nil {
+	//	args := []string{"no-pdf-compression"}
+	//	ok, _ := r.GeneratePDF(outputPath, args)
+	//	fmt.Println(ok, "pdf generated successfully")
+	//} else {
+	//	fmt.Println(err)
+	//}
+	//
+	//templatePath = "blankPuppyCard.html"
+	////templatePath := "blank4.html"
+	//outputPath = fmt.Sprintf("Щенячка_%s.pdf", gaplanPC.Nickname)
+	//if err := r.ParseTemplate(templatePath, gaplanPC); err == nil {
+	//	args := []string{"no-pdf-compression"}
+	//	ok, _ := r.GeneratePDF(outputPath, args)
+	//	fmt.Println(ok, "pdf generated successfully")
+	//} else {
+	//	fmt.Println(err)
+	//}
+
+	http.HandleFunc("/", handleRequest)
+	fmt.Println("Server listening on port 3333")
+	log.Fatal(http.ListenAndServe(":3333", nil))
+}
+
+func handleRequest(w http.ResponseWriter, r *http.Request) {
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		log.Println("Failed to read request body:", err)
+		return
 	}
+	defer r.Body.Close()
 
-	mother := Dog{
-		Type:     "среднеазиатская овчарка",
-		Nickname: "Анзурат Майсун Ф-Айпери",
-		Sex:      "c",
-		Stamp:    "CKF 4707",
-		Pedigree: "5582067 Р",
-		Owner: &Owner{
-			FIO:     "Каратаева Е.В",
-			Contact: "142660 МО, Орехово-Зуевский р-н, Савостьяново, 66",
-		},
-		Breeder: &Breeder{
-			FIO:     "Каратаева Е.В",
-			Name:    "Анзурат Майсун",
-			Contact: "142660 МО, Орехово-Зуевский р-н, Савостьяново, 66",
-			Email:   "breeder@email.com",
-		},
-	}
+	if r.Method == "POST" {
+		t := time.Now().Unix()
+		defer os.Remove(strconv.FormatInt(t, 10) + "/" + "Акт вязки.pdf")
+		defer os.Remove(strconv.FormatInt(t, 10) + "/" + "СогласиеПД.pdf")
+		defer os.Remove(strconv.FormatInt(t, 10) + "/" + "Обследование.pdf")
+		defer os.Remove(strconv.FormatInt(t, 10) + "/" + "Регистрация.pdf")
 
-	gaplan := Puppy{
-		Dog: Dog{
-			Type:     "среднеазиатская овчарка",
-			Nickname: "Гаплан",
-			Sex:      "к",
-			Stamp:    "CKF 6059",
-			Pedigree: "",
-			Owner: &Owner{
-				FIO:     " ",
-				Contact: " ",
-			},
-			Breeder: &Breeder{
-				FIO:     "Каратаева Е.В",
-				Contact: "142660 МО, Орехово-Зуевский р-н, Савостьяново, 66",
-				Name:    "Анзурат Майсун",
-				Email:   "tula-skif@mail.ru",
-			},
-			Father: &father,
-			Mother: &mother,
-		},
-		Color:    "бел-чер",
-		Birthday: "28.03.2023",
-		WoolType: "",
-		Description: struct {
-			Comment  string
-			Defect   bool
-			Revision int
-		}{"Замечаний нет", false, 0},
-		IncreaseIndex: func(a int) int {
-			return a + 1
-		},
-	}
+		Data := Input{}
+		r := pdf.NewRequestPdf("")
+		if err := json.Unmarshal(body, &Data); err != nil {
+			fmt.Fprintf(w, "Received error: %s", err)
+			w.WriteHeader(http.StatusNotAcceptable)
+		}
+		mating := Mating{}
+		mating.Construct(Data)
+		litter := Litter{}
+		litter.Construct(Data)
 
-	galsan := Puppy{
-		Dog: Dog{
-			Type:     "среднеазиатская овчарка",
-			Nickname: "Гаплан",
-			Sex:      "к",
-			Stamp:    "CKF 6060",
-			Pedigree: "",
-			Owner: &Owner{
-				FIO:     "Каратаева Е.В",
-				Contact: "142660 МО, Орехово-Зуевский р-н, Савостьяново, 66",
-			},
-		},
-		Color:    "бел-чер",
-		Birthday: "28 марта 2023",
-		WoolType: "",
-		Description: struct {
-			Comment  string
-			Defect   bool
-			Revision int
-		}{"Замечаний нет", false, 0},
-		IncreaseIndex: func(a int) int {
-			return a + 1
-		},
-	}
+		if _, err := os.Stat(strconv.FormatInt(t, 10) + "/"); os.IsNotExist(err) {
+			errDir := os.Mkdir(strconv.FormatInt(t, 10)+"/", 0777)
+			if errDir != nil {
+				http.Error(w, "Internal server error", 500)
+				return
+			}
+		}
 
-	ganzhin := Puppy{
-		Dog: Dog{
-			Type:     "среднеазиатская овчарка",
-			Nickname: "Гаплан",
-			Sex:      "с",
-			Stamp:    "CKF 6064",
-			Pedigree: "",
-			Owner: &Owner{
-				FIO:     "Каратаева Е.В",
-				Contact: "142660 МО, Орехово-Зуевский р-н, Савостьяново, 66",
-			},
-		},
-		Color:    "бел-чер",
-		Birthday: "28 марта 2023",
-		WoolType: "",
-		Description: struct {
-			Comment  string
-			Defect   bool
-			Revision int
-		}{"Замечаний нет", false, 0},
-		IncreaseIndex: func(a int) int {
-			return a + 1
-		},
-	}
+		zipFile, err := os.Create(strconv.FormatInt(t, 10) + "/" + "archive.zip")
+		if err != nil {
+			http.Error(w, "Internal server error", 500)
+			return
+		}
+		defer zipFile.Close()
 
-	skiff := DocumentCenter{
-		Name:         "ТООО «Кинологический центр «СКИФ» г. Тула",
-		Email:        "tula-skif@mail.ru",
-		Mobile:       "+7 910 701-55-99",
-		Federation:   "РФСС",
-		FolderNumber: "7107",
-		Address:      "300025 Тула, проспект Ленина, 104-201",
-	}
+		zipWriter := zip.NewWriter(zipFile)
+		defer zipWriter.Close()
 
-	instructor := Personal{
-		"Девятиярова Е.А.",
-		"Тульская обл, Ленинский р-н, Нижняя Китаевка, 24",
-	}
+		templatePath := "blankMating.html"
+		outputPath := fmt.Sprintf(strconv.FormatInt(t, 10) + "/" + "Акт вязки.pdf")
+		if err := r.ParseTemplate(templatePath, mating); err == nil {
+			args := []string{"no-pdf-compression"}
+			ok, _ := r.GeneratePDF(outputPath, args)
+			fmt.Println(ok, "pdf generated successfully")
+		} else {
+			fmt.Fprintf(w, "Received error: %s", err)
+			w.WriteHeader(http.StatusInternalServerError)
+		}
 
-	stumpMan := Personal{"Аргунова Л.А.", ""}
+		pdfFile, err := os.Open(outputPath)
+		if err != nil {
+			http.Error(w, "Internal server error", 500)
+			return
+		}
+		defer pdfFile.Close()
 
-	data := Mating{
-		Identification: "31 декабря 2022",
-		First:          "01 января 2023",
-		Control:        "02 января 2023",
-		Address:        "Донецк, Флотская, 68",
-		Male:           &father,
-		Female:         &mother,
-		DCenter:        &skiff,
-		Personal:       &instructor,
-	}
+		zipEntry, err := zipWriter.Create("Акт вязки.pdf")
+		if err != nil {
+			http.Error(w, "Internal server error", 500)
+			return
+		}
+		_, err = io.Copy(zipEntry, pdfFile)
+		if err != nil {
+			http.Error(w, "Internal server error", 500)
+			return
+		}
 
-	litter1 := Litter{
-		Stump:    "CKF",
-		ActDate:  "20 августа 1944",
-		Birthday: "16 декабря 2016",
-		Control:  "",
-		Address:  "",
-		Male:     &father,
-		Female:   &mother,
-		Puppy:    []*Puppy{&gaplan, &galsan, &ganzhin},
-		DCenter:  &skiff,
-		Personal: &instructor,
-		Claim:    &stumpMan,
-		Chief:    &instructor,
-	}
+		templatePath = "blankAgreement.html"
+		outputPath = fmt.Sprintf(strconv.FormatInt(t, 10) + "/" + "СогласиеПД.pdf")
+		if err := r.ParseTemplate(templatePath, mating); err == nil {
+			args := []string{"no-pdf-compression"}
+			ok, _ := r.GeneratePDF(outputPath, args)
+			fmt.Println(ok, "pdf generated successfully")
+		} else {
+			fmt.Fprintf(w, "Received error: %s", err)
+			w.WriteHeader(http.StatusInternalServerError)
+		}
 
-	gaplanPC := PuppyCard{
-		Puppy:   gaplan,
-		DCenter: skiff,
-	}
+		pdfFile, err = os.Open(outputPath)
+		if err != nil {
+			http.Error(w, "Internal server error", 500)
+			return
+		}
+		defer pdfFile.Close()
 
-	templatePath := "blankMating.html"
-	//templatePath := "blank4.html"
-	outputPath := fmt.Sprintf("%s_акт_вязки_%s.pdf", data.Personal.FIO, data.Female.Nickname)
-	if err := r.ParseTemplate(templatePath, data); err == nil {
-		args := []string{"no-pdf-compression"}
-		ok, _ := r.GeneratePDF(outputPath, args)
-		fmt.Println(ok, "pdf generated successfully")
+		zipEntry, err = zipWriter.Create("СогласиеПД.pdf")
+		if err != nil {
+			http.Error(w, "Internal server error", 500)
+			return
+		}
+		_, err = io.Copy(zipEntry, pdfFile)
+		if err != nil {
+			http.Error(w, "Internal server error", 500)
+			return
+		}
+
+		templatePath = "blankSurvey.html"
+		outputPath = fmt.Sprintf(strconv.FormatInt(t, 10) + "/" + "Обследование.pdf")
+		if err := r.ParseTemplate(templatePath, litter); err == nil {
+			args := []string{"no-pdf-compression"}
+			ok, _ := r.GeneratePDF(outputPath, args)
+			fmt.Println(ok, "pdf generated successfully")
+		} else {
+			fmt.Fprintf(w, "Received error: %s", err)
+			w.WriteHeader(http.StatusInternalServerError)
+		}
+
+		pdfFile, err = os.Open(outputPath)
+		if err != nil {
+			http.Error(w, "Internal server error", 500)
+			return
+		}
+		defer pdfFile.Close()
+
+		zipEntry, err = zipWriter.Create("Обследование.pdf")
+		if err != nil {
+			http.Error(w, "Internal server error", 500)
+			return
+		}
+		_, err = io.Copy(zipEntry, pdfFile)
+		if err != nil {
+			http.Error(w, "Internal server error", 500)
+			return
+		}
+
+		templatePath = "blankRegister.html"
+		outputPath = fmt.Sprintf(strconv.FormatInt(t, 10) + "/" + "Регистрация.pdf")
+		if err := r.ParseTemplate(templatePath, litter); err == nil {
+			args := []string{"no-pdf-compression"}
+			ok, _ := r.GeneratePDF(outputPath, args)
+			fmt.Println(ok, "pdf generated successfully")
+		} else {
+			fmt.Fprintf(w, "Received error: %s", err)
+			w.WriteHeader(http.StatusInternalServerError)
+		}
+
+		pdfFile, err = os.Open(outputPath)
+		if err != nil {
+			http.Error(w, "Internal server error", 500)
+			return
+		}
+		defer pdfFile.Close()
+
+		zipEntry, err = zipWriter.Create("Регистрация.pdf")
+		if err != nil {
+			http.Error(w, "Internal server error", 500)
+			return
+		}
+		_, err = io.Copy(zipEntry, pdfFile)
+		if err != nil {
+			http.Error(w, "Internal server error", 500)
+			return
+		}
+
+		w.Header().Set("Content-Type", "application/zip")
+		w.Header().Set("Content-Disposition", "attachment; filename=matingAct.pdf")
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+
+		_, err = io.Copy(w, zipFile)
+		if err != nil {
+			http.Error(w, "Internal server error", 500)
+			return
+		}
+		fmt.Println("pdf copied successfully")
+
 	} else {
-		fmt.Println(err)
-	}
-
-	templatePath = "blankAgreement.html"
-	//templatePath := "blank4.html"
-	outputPath = fmt.Sprintf("%s_согласие_ПД_%s.pdf", data.Personal.FIO, data.Female.Nickname)
-	if err := r.ParseTemplate(templatePath, data); err == nil {
-		args := []string{"no-pdf-compression"}
-
-		ok, _ := r.GeneratePDF(outputPath, args)
-		fmt.Println(ok, "pdf generated successfully")
-	} else {
-		fmt.Println(err)
-	}
-
-	templatePath = "blankSurvey.html"
-	//templatePath := "blank4.html"
-	outputPath = fmt.Sprintf("%s_обследование_%s.pdf", litter1.Personal.FIO, litter1.Birthday)
-	if err := r.ParseTemplate(templatePath, litter1); err == nil {
-		args := []string{"no-pdf-compression"}
-		ok, _ := r.GeneratePDF(outputPath, args)
-		fmt.Println(ok, "pdf generated successfully")
-	} else {
-		fmt.Println(err)
-	}
-
-	templatePath = "blankRegister.html"
-	//templatePath := "blank4.html"
-	outputPath = fmt.Sprintf("%s_регистрация_%s.pdf", litter1.Personal.FIO, litter1.Birthday)
-	if err := r.ParseTemplate(templatePath, litter1); err == nil {
-		args := []string{"no-pdf-compression"}
-		ok, _ := r.GeneratePDF(outputPath, args)
-		fmt.Println(ok, "pdf generated successfully")
-	} else {
-		fmt.Println(err)
-	}
-
-	templatePath = "blankPuppyCard.html"
-	//templatePath := "blank4.html"
-	outputPath = fmt.Sprintf("Щенячка_%s.pdf", gaplanPC.Nickname)
-	if err := r.ParseTemplate(templatePath, gaplanPC); err == nil {
-		args := []string{"no-pdf-compression"}
-		ok, _ := r.GeneratePDF(outputPath, args)
-		fmt.Println(ok, "pdf generated successfully")
-	} else {
-		fmt.Println(err)
+		// Отправка заголовков CORS
+		w.Header().Set("Access-Control-Allow-Origin", "*") // Здесь можно указать конкретный источник
+		w.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+		//Отправка ответа клиенту
+		w.WriteHeader(http.StatusOK)
 	}
 
 }
--- models.go
+++ models.go
@@ -62,10 +62,12 @@ type Owner struct {
 }
 
 type Breeder struct {
-	FIO     string
-	Name    string
-	Contact string
-	Email   string
+	FIO          string
+	Name         string
+	NameEng      string
+	NamePosition string
+	Contact      string
+	Email        string
 }
 
 type Dog struct {
@@ -87,8 +89,8 @@ type Puppy struct {
 	WoolType    string
 	Description struct {
 		Comment  string
-		Defect   bool
-		Revision int
+		Defect   string
+		Revision string
 	}
 	IncreaseIndex func(a int) int
 }
@@ -138,7 +140,7 @@ func (l Litter) ShowPuppyDefected(checkbox bool) int {
 	var counter int
 
 	for _, v := range l.Puppy {
-		if v.Description.Defect == checkbox {
+		if v.Description.Defect == "defect" {
 			counter++
 		}
 	}
@@ -155,4 +157,200 @@ func (p Puppy) ShowStumpNumber() int {
 	}
 
 	return 0
+}
+
+type Input struct {
+	NameDC          string `json:"NameDC"`
+	AddressDC       string `json:"AddressDC"`
+	EmailDC         string `json:"EmailDC"`
+	Mobile          string `json:"Mobile"`
+	Federation      string `json:"Federation"`
+	FolderNumber    string `json:"FolderNumber"`
+	StumpCode       string `json:"StumpCode"`
+	ChiefFIO        string `json:"ChiefFIO"`
+	ChiefAddress    string `json:"ChiefAddress"`
+	PersonalFIO     string `json:"PersonalFIO"`
+	PersonalAddress string `json:"PersonalAddress"`
+	ClaimFIO        string `json:"ClaimFIO"`
+	ClaimAddress    string `json:"ClaimAddress"`
+	Identification  string `json:"Identification"`
+	First           string `json:"First"`
+	Control         string `json:"Control"`
+	MatingAddress   string `json:"MatingAddress"`
+	LitterBirthday  string `json:"LitterBirthday"`
+	LitterSurvey    string `json:"LitterSurvey"`
+	RevisionPeriod  string `json:"RevisionPeriod"`
+	FatherName      string `json:"FatherName"`
+	FatherPedigree  string `json:"FatherPedigree"`
+	FatherStump     string `json:"FatherStump"`
+	FatherOwnerFIO  string `json:"FatherOwnerFIO"`
+	FatherAddress   string `json:"FatherAddress"`
+	MotherName      string `json:"MotherName"`
+	MotherPedigree  string `json:"MotherPedigree"`
+	MotherStump     string `json:"MotherStump"`
+	MotherType      string `json:"MotherType"`
+	Suffix          string `json:"Suffix"`
+	BreederFIO      string `json:"BreederFIO"`
+	BreederContact  string `json:"BreederContact"`
+	BreederEmail    string `json:"BreederEmail"`
+	BreederName     string `json:"BreederName"`
+	BreederNameEnd  string `json:"BreederNameEnd"`
+	Puppy           []struct {
+		Nickname      string `json:"Nickname"`
+		Color         string `json:"Color"`
+		WoolType      string `json:"WoolType"`
+		PuppyStump    string `json:"PuppyStump"`
+		SexPuppy      string `json:"SexPuppy"`
+		StatusPuppy   string `json:"SexPuppy"`
+		StatusComment string `json:"SexPuppy"`
+	} `json:"Puppy"`
+}
+
+func (m *Mating) Construct(i Input) {
+	*m = Mating{
+		Identification: i.Identification,
+		First:          i.First,
+		Control:        i.Control,
+		Address:        i.MatingAddress,
+		Male: &Dog{
+			Type:     i.MotherType,
+			Nickname: i.FatherName,
+			Sex:      "к",
+			Stamp:    i.FatherStump,
+			Pedigree: i.FatherPedigree,
+			Owner: &Owner{
+				FIO:     i.FatherOwnerFIO,
+				Contact: i.FatherAddress,
+			},
+		},
+		Female: &Dog{
+			Type:     i.MotherType,
+			Nickname: i.MotherName,
+			Sex:      "с",
+			Stamp:    i.MotherStump,
+			Pedigree: i.MotherPedigree,
+			Breeder: &Breeder{
+				FIO:          i.BreederFIO,
+				Name:         i.BreederName,
+				NameEng:      i.BreederNameEnd,
+				NamePosition: i.Suffix,
+				Contact:      i.BreederContact,
+				Email:        i.BreederEmail,
+			},
+			Owner: &Owner{
+				FIO:     i.BreederFIO,
+				Contact: i.BreederContact,
+				Email:   i.BreederEmail,
+			},
+		},
+		DCenter: &DocumentCenter{
+			Name:         i.NameDC,
+			Email:        i.EmailDC,
+			Mobile:       i.Mobile,
+			Federation:   i.Federation,
+			FolderNumber: i.FolderNumber,
+			StumpCode:    i.StumpCode,
+			Address:      i.AddressDC,
+		},
+		Personal: &Personal{
+			FIO:     i.PersonalFIO,
+			Address: i.PersonalAddress,
+		},
+	}
+}
+
+func (l *Litter) Construct(i Input) {
+	*l = Litter{
+		Stump:    i.StumpCode,
+		ActDate:  i.LitterSurvey,
+		Birthday: i.LitterBirthday,
+		Control:  i.Control,
+		Address:  i.MatingAddress,
+		Male: &Dog{
+			Type:     i.MotherType,
+			Nickname: i.FatherName,
+			Sex:      "к",
+			Stamp:    i.FatherStump,
+			Pedigree: i.FatherPedigree,
+			Owner: &Owner{
+				FIO:     i.FatherOwnerFIO,
+				Contact: i.FatherAddress,
+			},
+		},
+		Female: &Dog{
+			Type:     i.MotherType,
+			Nickname: i.MotherName,
+			Sex:      "с",
+			Stamp:    i.MotherStump,
+			Pedigree: i.MotherPedigree,
+			Breeder: &Breeder{
+				FIO:          i.BreederFIO,
+				Name:         i.BreederName,
+				NameEng:      i.BreederNameEnd,
+				NamePosition: i.Suffix,
+				Contact:      i.BreederContact,
+				Email:        i.BreederEmail,
+			},
+			Owner: &Owner{
+				FIO:     i.BreederFIO,
+				Contact: i.BreederContact,
+				Email:   i.BreederEmail,
+			},
+		},
+		Puppy: nil,
+		DCenter: &DocumentCenter{
+			Name:         i.NameDC,
+			Email:        i.EmailDC,
+			Mobile:       i.Mobile,
+			Federation:   i.Federation,
+			FolderNumber: i.FolderNumber,
+			StumpCode:    i.StumpCode,
+			Address:      i.AddressDC,
+		},
+		Personal: &Personal{
+			FIO:     i.PersonalFIO,
+			Address: i.PersonalAddress,
+		},
+		Claim: &Personal{
+			FIO:     i.ClaimFIO,
+			Address: i.ClaimAddress,
+		},
+		Chief: &Personal{
+			FIO:     i.ChiefFIO,
+			Address: i.ChiefAddress,
+		},
+	}
+
+	for _, v := range i.Puppy {
+		tmp := Puppy{
+			Dog: Dog{
+				Type:     i.MotherType,
+				Nickname: v.Nickname,
+				Sex:      v.SexPuppy,
+				Stamp:    v.PuppyStump,
+				Pedigree: "",
+				Father:   l.Male,
+				Mother:   l.Female,
+				Breeder:  l.Female.Breeder,
+				Owner:    nil,
+			},
+			Color:    v.Color,
+			Birthday: i.LitterBirthday,
+			WoolType: v.WoolType,
+			Description: struct {
+				Comment  string
+				Defect   string
+				Revision string
+			}{
+				v.StatusComment,
+				v.StatusPuppy,
+				i.RevisionPeriod,
+			},
+			IncreaseIndex: func(a int) int {
+				return a + 1
+			},
+		}
+		l.Puppy = append(l.Puppy, &tmp)
+	}
+
 }
